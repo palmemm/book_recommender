bb546b3863410ed13dd4280ada18c42d2e5bbbf2
palmemm <166724934+palmemm@users.noreply.github.com>
2025-11-21 08:55:53 -0700

    fixed memory issues

--- data_cleanup.py
+++ data_cleanup.py
@@ -1,29 +1,46 @@
 import pandas as pd
 import json
+from scipy.sparse import lil_matrix
+import numpy as np
 from api_request import get_one_book
 
-user_ratings = {}
-book_title_cache = 'book_title_cache.json'
+user_to_idx = {'Average':0}
+book_to_idx = {}
+# book_title_cache = 'book_title_cache.json'
+book_titles = {}
 
-with open(book_title_cache, 'w') as f:
+# with open(book_title_cache, 'w') as f:
 
-    for book in get_one_book():
-        book_id = book['id']
-        f.write(json.dumps({book_id:book['title']}) + '\n')
-        user_ratings['Average'] = {book_id:book['rating']}
-        for user in book['user_books']:
-            user_id = user['user_id']
-            if user_id in user_ratings.keys():
-                user_ratings[user_id][book_id] = user['rating']
-            else:
-                user_ratings[user_id] = {book_id:user['rating']}
+for book in get_one_book():
+    book_id = book['id']
+    if book_id not in book_to_idx:
+        book_to_idx[book_id] = len(book_to_idx)
+    book_titles[book_id] = book['title']
+    # f.write(json.dumps({book_id:book['title']}) + '\n')
+    
+    for user in book['user_books']:
+        user_id = user['user_id']
+        if user_id not in user_to_idx:
+            user_to_idx[user_id] = len(user_to_idx)
 
-rating_df = pd.DataFrame(user_ratings)
+n_books = len(book_to_idx)
+n_users = len(user_to_idx)
+ratings_matrix = lil_matrix((n_books, n_users), dtype=np.float32)
+
+for book in get_one_book():
+    book_idx = book_to_idx[book['id']]
+    ratings_matrix[book_idx, 0] = book['rating']
+    for user in book['user_books']:
+        user_idx = user_to_idx[user['user_id']]
+        ratings_matrix[book_idx, user_idx] = user['rating']
+        
+rating_df = pd.DataFrame.sparse.from_spmatrix(ratings_matrix, columns=user_to_idx.keys(), index=book_to_idx.keys())
 print(rating_df.info())
 
-#Want to create a user-item matrix so will take the transpose. Then will delete all rows with only null values
+#Want to create a user-item matrix so will take the transpose. First will delete all users with only null/0 values
+col_sums = rating_df.sum(axis=0)
+rating_df = rating_df.loc[:, col_sums>0]
 rating_df = rating_df.T
 
-rating_df = rating_df.dropna(axis=0, how='all') 
-rating_df = rating_df.dropna(axis=1, how='all')
 print(rating_df.info())
+print(rating_df.head(10))
--- my_recommender_pipeline.py
+++ my_recommender_pipeline.py
@@ -1,14 +1,16 @@
+import json
 from data_cleanup import rating_df, book_titles
 
-# This file will recommend books to a user based on their top-rated books, and will show them books 
+# This file will recommend books to me based on my top-rated books, and will show me books 
 # that other users who rated those books highly rated highly.
 
-# Step 1: Pick a random user
+# Step 1: Extract my user id
 # Since this is just for funsies, will have it pull my user id and will recommend books to myself. 
 # In theory, this could be applied to any user in the df
 
 my_id = 53322
 
+#Step 2: Extract books I rated 4 stars or higher and pull the titles to see which books made the cut
 my_ratings = rating_df.loc[my_id]
 
 liked_books = []
@@ -16,6 +18,15 @@ for book_id,rating in my_ratings.items():
     if rating >= 4:
         liked_books.append(book_id)
 
+# book_titles = []
+# with open('book_title_cache.json', 'r') as f:
+#     for line in f:
+#         book_titles = json.loads(line)
+
+
 liked_book_titles = [book_titles[book_id] for book_id in liked_books]
+
 print(len(liked_book_titles))
 print(liked_book_titles)
+
+#Step 3: Find other users who rated those books highly
